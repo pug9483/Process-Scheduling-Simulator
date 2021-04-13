6a7f73b0b3daa81f70ff36ade96121c559725374
woosangbum <67853792+woosangbum@users.noreply.github.com>
2021-04-13 12:15:09 +0900

    fcfs, spn 멀티프로세서 구현완료
    fcfs, spn 멀티프로세서 구현완료fcfs, spn 멀티프로세서 구현완료

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const body = document.querySelector("body");
 let numberOfProcess;  // 총 프로세스 수
 let numberOfProcessor;  // 프로세서 수
 let processData = new Array(); /* 각 프로세스 별 정보:프로세스번호, 도착시간, 
-실행시간, 시작시간, 종료시간, 대기시간}*/
+실행시간, 시작시간, 종료시간, 대기시간, 할당된 프로세서 번호, 잔여시간}*/
 let quantumTime; // 퀀텀 타임
-let processorState = new Array; // 프로세서 별 실행중인지 아닌지 확인하기 위한 변수
-let processorData = new Array;
+let processorState = new Array(4); // 프로세서 별 실행중인지 아닌지 확인하기 위한 변수
+let processorData = new Array(4); // 각 프로세서 별 실행중인 프로세스
 let checkRun = -1; // run()이 실행되면 1로 바뀜
 
 //-----------------전역변수 선언 끝--------------------
@@ -150,32 +150,34 @@ function chooseProcessAlgorithm(){
 // 알고리즘 6개
 function fcfs(){ 
     const readyQueue = new Queue();
-    let processorRunningQueue = new Queue(); // 프로세서 실행 큐
     let presentTime = -1; // 현재시간 -1으로 초기화
     let exitCount = 0; // 종료된 프로세스 개수
     let dequeProcess = new Array();
-    let runningProcess = 1;
     let totoalTime;
-    // 각 프로세서데이터에 프로세서실행큐를 할당
-    for(let i=0;i<numberOfProcessor;i++){
-        //우선 프로세서가 하나일 때만 고려
-        processorData[0] = processorRunningQueue;//procesoorData는 프로세서들의 각 실행큐를 모아둔 배열
-    }
+    let runningProcess = new Array();
+    
     
     const cloneprocessData = processData; // processData 클론 생성
     
-    while(checkRun == 1 && exitCount < numberOfProcess){ // run이 눌리면 시간 진행 && 종료프로세스수가 총 프로세스수보다 작을때
+    while(checkRun == 1 && exitCount < numberOfProcess){ // run이 눌리면 시간 진행 && 모든 프로세스가 종료되었을 때
         presentTime++; // 현재시간 1추가   
         console.log("presentime: ",presentTime);
         console.log("presentReadyQueue: ",readyQueue.toString());  // 레디큐 확인
         
-        //시작시간 + 실행시간이 현재시간일 때
-        if((Number(cloneprocessData[runningProcess-1][2])+Number(cloneprocessData[runningProcess-1][3]) == presentTime)&&
-        processorState == 1){
-            let exitProcess = processorData[0].dequeue();
-            console.log("================= P"+exitProcess[0],"종료 =================");
-            exitCount++;
-            processorState[0] = -1; // 프로세서를 종료한다.
+        for(let i =0; i<numberOfProcessor;i++){         
+            //(프로세스 종료조건)시작시간 + 실행시간이 현재시간일 때
+            for(let j = 0; j< runningProcess.length; j++){
+                console.log("runningprocess",runningProcess);
+                if((Number(cloneprocessData[runningProcess[j][0]][2])+Number(cloneprocessData[runningProcess[j][0]][3]) == presentTime)&&
+                processorState[i] == 1){                
+                    console.log("================= P"+(runningProcess[j][0]+1),"종료 =================");
+                    processorData[runningProcess[j][1]] = -1;
+                    runningProcess.shift(j);
+                    exitCount++;
+                    processorState[i] = -1; // 프로세서를 종료한다.
+                }
+            }
+            
         }
         
         for(let i=0;i<numberOfProcess; i++){ // 현재시간 == 도착시간인 프로세스가 있으면 레디큐에 삽입
@@ -185,17 +187,27 @@ function fcfs(){
         }
 
         
-        // 프로세서가 현재 작동중이 아니면 && 레디큐에 프로세스가 있으면
-        if(readyQueue.empty() == false && processorState[0] == -1){ 
-            processorState[0] = 1// 프로세서를 작동시킨다
-            dequeProcess = readyQueue.dequeue()
-            processorData[0].enqueue(dequeProcess); // 레디큐에서 하나를 데큐해서 프로세서 실행큐에 삽입
-            runningProcess = dequeProcess[0];
-            cloneprocessData[runningProcess-1][3] = presentTime; // 프로세스의 시작시간을 현재시간으로 설정
+        //  레디큐에 프로세스가 있으면 && 작동 가능한 프로세서가 있다면
+        while(readyQueue.empty() == false && processorState.indexOf(-1) >= 0){ 
+            
+            // 꺼져있는 프로세서 중 가장 앞에 있는 프로세서의 인덱스를 반환
+            let workSpace = processorState.indexOf(-1); 
+            processorState[workSpace] = 1; // 프로세서를 작동시킨다
+            dequeProcess = readyQueue.dequeue();
+            processorData[workSpace] = "P"+dequeProcess[0]; 
+            runningProcess.push([dequeProcess[0]-1,workSpace]);
+            cloneprocessData[dequeProcess[0]-1][3] = presentTime; // 프로세스의 시작시간을 현재시간으로 설정
+            cloneprocessData[dequeProcess[0]-1][6] = workSpace;
         } 
-        
-        
-        console.log("presentProceesorQueue: ",processorData[0].toString());
+        for(let i =0; i< numberOfProcessor; i++){
+            if(processorData[i] == -1){
+                console.log("presentProceesor"+i+" Queue: Empty.");
+            }
+            else{
+                console.log("presentProceesor"+i+" Queue: ",processorData[i]);
+            }
+        }
+        if(presentTime >100) break; // 무한 루프 방지
         
         
     }
@@ -206,32 +218,36 @@ function rr(){
     
 }
 function spn(){
+    
     const readyQueue = new Queue();
-    let processorRunningQueue = new Queue(); // 프로세서 실행 큐
     let presentTime = -1; // 현재시간 -1으로 초기화
     let exitCount = 0; // 종료된 프로세스 개수
     let dequeProcess = new Array();
-    let runningProcess = 1;
-    // 각 프로세서데이터에 프로세서실행큐를 할당
-    for(let i=0;i<numberOfProcessor;i++){
-        //우선 프로세서가 하나일 때만 고려
-        processorData[0] = processorRunningQueue;//procesoorData는 프로세서들의 각 실행큐를 모아둔 배열
-    }
+    let totoalTime;
+    let runningProcess = new Array();
+    
     
     const cloneprocessData = processData; // processData 클론 생성
     
-    while(checkRun == 1 && exitCount < numberOfProcess){ // run이 눌리면 시간 진행 && 종료프로세스수가 총 프로세스수보다 작을때
+    while(checkRun == 1 && exitCount < numberOfProcess){ // run이 눌리면 시간 진행 && 모든 프로세스가 종료되었을 때
         presentTime++; // 현재시간 1추가   
         console.log("presentime: ",presentTime);
         console.log("presentReadyQueue: ",readyQueue.toString());  // 레디큐 확인
         
-        //시작시간 + 실행시간이 현재시간일 때
-        if((Number(cloneprocessData[runningProcess-1][2])+Number(cloneprocessData[runningProcess-1][3]) == presentTime)&&
-        processorState == 1){
-            let exitProcess = processorData[0].dequeue();
-            console.log("================= P"+exitProcess[0],"종료 =============");
-            exitCount++;
-            processorState[0] = -1; // 프로세서를 종료한다.
+        for(let i =0; i<numberOfProcessor;i++){         
+            //(프로세스 종료조건)시작시간 + 실행시간이 현재시간일 때
+            for(let j = 0; j< runningProcess.length; j++){
+                console.log("runningprocess",runningProcess);
+                if((Number(cloneprocessData[runningProcess[j][0]][2])+Number(cloneprocessData[runningProcess[j][0]][3]) == presentTime)&&
+                processorState[i] == 1){                
+                    console.log("================= P"+(runningProcess[j][0]+1),"종료 =================");
+                    processorData[runningProcess[j][1]] = -1;
+                    runningProcess.shift(j);
+                    exitCount++;
+                    processorState[i] = -1; // 프로세서를 종료한다.
+                }
+            }
+            
         }
         
         for(let i=0;i<numberOfProcess; i++){ // 현재시간 == 도착시간인 프로세스가 있으면 레디큐에 삽입
@@ -240,26 +256,36 @@ function spn(){
             }
         }
         
-        // bursttime에 따라 레디큐 오른차순 정렬
-        readyQueue.spnSort()
         
-        
-        
-        // 프로세서가 현재 작동중이 아니면 && 레디큐에 프로세스가 있으면
-        if(readyQueue.empty() == false && processorState[0] == -1){ 
-            processorState[0] = 1// 프로세서를 작동시킨다
-            dequeProcess = readyQueue.dequeue()
-            processorData[0].enqueue(dequeProcess); // 레디큐에서 하나를 데큐해서 프로세서 실행큐에 삽입
-            runningProcess = dequeProcess[0];
-            cloneprocessData[runningProcess-1][3] = presentTime; // 프로세스의 시작시간을 현재시간으로 설정
+        //  레디큐에 프로세스가 있으면 && 작동 가능한 프로세서가 있다면
+        while(readyQueue.empty() == false && processorState.indexOf(-1) >= 0){ 
+            readyQueue.spnSort()
+            
+            // 꺼져있는 프로세서 중 가장 앞에 있는 프로세서의 인덱스를 반환
+            let workSpace = processorState.indexOf(-1); 
+            processorState[workSpace] = 1; // 프로세서를 작동시킨다
+            dequeProcess = readyQueue.dequeue();
+            processorData[workSpace] = "P"+dequeProcess[0]; 
+            runningProcess.push([dequeProcess[0]-1,workSpace]);
+            cloneprocessData[dequeProcess[0]-1][3] = presentTime; // 프로세스의 시작시간을 현재시간으로 설정
+            cloneprocessData[dequeProcess[0]-1][6] = workSpace;
         } 
-        
-        
-        console.log("presentProceesorQueue: ",processorData[0].toString());
+        for(let i =0; i< numberOfProcessor; i++){
+            if(processorData[i] == -1){
+                console.log("presentProceesor"+i+" Queue: Empty.");
+            }
+            else{
+                console.log("presentProceesor"+i+" Queue: ",processorData[i]);
+            }
+        }
+        if(presentTime >100) break; // 무한 루프 방지
         
         
     }
+    totoalTime = presentTime; //전체실행시간을 저장.
+    console.log("Totaltime: ",presentTime);
 }
+
 function sptn(){
     
 }
@@ -352,9 +378,9 @@ function run(){
     
     //프로세스 정보를 넣을 2차원 배열 생성하기
     for(let i = 0; i<inputTable.rows.length; i++){
-        processData[i] = new Array(6);
+        processData[i] = new Array(8);
     }
-    console.log("fristCreated:",processData);
+
     //프로세스번호, 도착시간(index = 1), 실행 시간(index = 2) 저장 배열
     for(let i=0; i <inputTable.rows.length; i++){
         processData[i][0] = (i+1);
@@ -368,7 +394,7 @@ function run(){
     console.log("Run-processData:",processData);
     console.log("quantumTime: ",quantumTime);
     console.log("Processor State: ",processorState);
-    
+    console.log("================run================");
     // 표 만들기 : 이름, Arrival Time, Buster Time, Wating Time, Turnaound Time, Nomarlized TT
     createShowTable();
 
